281f0c43d2f3d363b037f48beb50723914cc701d
Angelos Osafi <aggelososafi11@gmail.com>
2024-12-24 22:39:47 +0200

    refactor folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+// import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent], //RouterOutlet
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
